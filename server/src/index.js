cbe54bde77fd2b93e8af13466577619d8f5cdca2
olikat7 <olivierbaudoux@gmail.com>
2025-03-12 10:02:08 +0100

    Update env

--- server/src/index.js
+++ server/src/index.js
@@ -6,11 +6,11 @@ const authRoutes = require("./authRoutes");
 dotenv.config();
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001; // Utiliser le port fourni par Render
 
 app.use(cors());
 app.use("/auth", authRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Serveur démarré sur http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Serveur démarré sur le port ${PORT}`);
 });
